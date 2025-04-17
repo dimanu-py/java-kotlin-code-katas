512709ab72945c4c265f12bace5cd62fe78461d5
dimanu-py <diegomtz126@gmail.com>
2025-04-17 12:44:27 +0200

    🧪 test(tictactoe): add test to verify board stores the player that has marked the tile

--- tictactoe/solution/src/test/kotlin/org/dimanu/tictactoe/BoardShould.kt
+++ tictactoe/solution/src/test/kotlin/org/dimanu/tictactoe/BoardShould.kt
@@ -2,6 +2,7 @@ package org.dimanu.tictactoe
 
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class BoardShould {
@@ -15,8 +16,16 @@ class BoardShould {
 
     @Test
     fun `detect when a tile is marked`() {
-        board.playOn(Tile.TOP_LEFT)
+        board.playOn(Tile.TOP_LEFT, Player.PLAYER_X)
 
         assertTrue(board.isMarked(Tile.TOP_LEFT))
+    }
+
+    @Test
+    fun `be able to know which player has marked a tile`() {
+        board.playOn(Tile.TOP_LEFT, Player.PLAYER_X)
+
+        val playedPositions: List<Tile> = board.tilesPlayedBy(Player.PLAYER_X)
+        assertEquals(playedPositions, listOf(Tile.TOP_LEFT))
     }
 }
