b8a0e505fb9f32bba4588e3f0f2d222cda00a747
dimanu-py <diegomtz126@gmail.com>
2025-04-17 10:00:05 +0200

    ♻ refactor(trip-service): extract setUp method to clean up test

--- tripservice/solution/src/test/kotlin/org/dimanu/tripservice/TripServiceShould.kt
+++ tripservice/solution/src/test/kotlin/org/dimanu/tripservice/TripServiceShould.kt
@@ -6,13 +6,15 @@ import org.dimanu.tripservice.exception.UserNotLoggedInException
 import org.dimanu.tripservice.trip.TripRepository
 import org.dimanu.tripservice.trip.TripService
 import org.dimanu.tripservice.user.User
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 
 class TripServiceShould {
 
-    private val tripRepository = mockk<TripRepository>()
+    private lateinit var tripRepository: TripRepository
+    private lateinit var tripService: TripService
 
     private val guestUser = null
     private val anyUser = UserMother.any()
@@ -20,10 +22,15 @@ class TripServiceShould {
     private val canadaTrip = TripMother.any()
     private var loggedUser: User? = null
 
+    @BeforeEach
+    fun setUp() {
+        tripRepository = mockk<TripRepository>()
+        tripService = TripService(tripRepository)
+    }
+
     @Test
     fun `not logged in user cannot interact with application`() {
         this.loggedUser = guestUser
-        val tripService = TripService(tripRepository)
 
         assertThrows<UserNotLoggedInException> {
             tripService.getTripsByUser(loggedUser, anyUser)
@@ -33,7 +40,6 @@ class TripServiceShould {
     @Test
     fun `user gets no trips when is not friend with logged user`() {
         this.loggedUser = applicationUser
-        val tripService = TripService(tripRepository)
         val stranger = UserMother.any()
 
         val trips = tripService.getTripsByUser(loggedUser, stranger)
@@ -44,7 +50,6 @@ class TripServiceShould {
     @Test
     fun `user gets its friends trips`() {
         this.loggedUser = applicationUser
-        val tripService = TripService(tripRepository)
         val friend = UserMother.withFriendsAndTrips(friends = listOf(loggedUser!!), trips = listOf(canadaTrip))
         every { tripRepository.findTripsByUser(any()) } returns listOf(canadaTrip)
 
