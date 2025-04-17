ff940f98eea383490703d593745efd978fe48318
dimanu-py <diegomtz126@gmail.com>
2025-04-17 09:44:23 +0200

    ♻ refactor(trip-service): make TripDAO implement TripRepository interface

--- tripservice/solution/src/main/kotlin/org/dimanu/tripservice/trip/TripDAO.kt
+++ tripservice/solution/src/main/kotlin/org/dimanu/tripservice/trip/TripDAO.kt
@@ -3,10 +3,8 @@ package org.dimanu.tripservice.trip
 import org.dimanu.tripservice.exception.CollaboratorCallException
 import org.dimanu.tripservice.user.User
 
-class TripDAO {
-    companion object {
-        @JvmStatic fun findTripsByUser(user: User): List<Trip> {
+class TripDAO : TripRepository {
+    override fun findTripsByUser(user: User): List<Trip> {
             throw CollaboratorCallException("TripDAO should not be invoked on an unit test.")
         }
-    }
 }
