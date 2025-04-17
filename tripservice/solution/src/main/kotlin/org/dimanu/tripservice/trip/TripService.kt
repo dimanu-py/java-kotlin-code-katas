50b7b323ca05d87b0e8a99d8dd0740c668d0eddb
dimanu-py <diegomtz126@gmail.com>
2025-04-17 10:06:53 +0200

    ♻ refactor(trip-service): replace empty trip list creation with a predefined constant

--- tripservice/solution/src/main/kotlin/org/dimanu/tripservice/trip/TripService.kt
+++ tripservice/solution/src/main/kotlin/org/dimanu/tripservice/trip/TripService.kt
@@ -5,13 +5,15 @@ import org.dimanu.tripservice.user.User
 
 open class TripService(private val tripRepository: TripRepository) {
 
+    private val noTrips: List<Trip> = emptyList()
+
     fun getTripsByUser(loggedUser: User?, requestedUser: User): List<Trip> {
         verifyUserIsLoggedIn(loggedUser)
 
         if (requestedUser.isFriendWith(loggedUser!!)) {
             return tripRepository.findTripsByUser(requestedUser)
         }
-        return ArrayList<Trip>()
+        return noTrips
     }
 
     private fun verifyUserIsLoggedIn(user: User?) {
