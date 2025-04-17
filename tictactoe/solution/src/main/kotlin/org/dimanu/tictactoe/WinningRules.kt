0d1c6a6b619bcd9dcbdee09b4a3504711529f389
dimanu-py <diegomtz126@gmail.com>
2025-04-17 19:10:13 +0200

    ✨ feat(tictactoe): add vertical winning conditions to WinningRules and corresponding tests

--- tictactoe/solution/src/main/kotlin/org/dimanu/tictactoe/WinningRules.kt
+++ tictactoe/solution/src/main/kotlin/org/dimanu/tictactoe/WinningRules.kt
@@ -4,7 +4,10 @@ class WinningRules {
     private val winningRules: List<List<Tile>> = listOf(
         listOf(Tile.TOP_LEFT, Tile.TOP_RIGHT, Tile.TOP_MIDDLE),
         listOf(Tile.MIDDLE_LEFT, Tile.MIDDLE_MIDDLE, Tile.MIDDLE_RIGHT),
-        listOf(Tile.BOTTOM_LEFT, Tile.BOTTOM_MIDDLE, Tile.BOTTOM_RIGHT)
+        listOf(Tile.BOTTOM_LEFT, Tile.BOTTOM_MIDDLE, Tile.BOTTOM_RIGHT),
+        listOf(Tile.TOP_LEFT, Tile.MIDDLE_LEFT, Tile.BOTTOM_LEFT),
+        listOf(Tile.TOP_MIDDLE, Tile.MIDDLE_MIDDLE, Tile.BOTTOM_MIDDLE),
+        listOf(Tile.TOP_RIGHT, Tile.MIDDLE_RIGHT, Tile.BOTTOM_RIGHT),
     )
 
     fun matches(markedTiles: List<Tile>): Boolean {
--- tictactoe/solution/src/test/kotlin/org/dimanu/tictactoe/WinningRulesShould.kt
+++ tictactoe/solution/src/test/kotlin/org/dimanu/tictactoe/WinningRulesShould.kt
@@ -15,12 +15,27 @@ class WinningRulesShould {
 
     }
 
+    @ParameterizedTest
+    @MethodSource("verticalWinningRules")
+    fun `detect vertical columns wins`(markedTiles: List<Tile>) {
+        val winningRules = WinningRules()
+
+        assertTrue(winningRules.matches(markedTiles))
+    }
+
     companion object {
         @JvmStatic
         fun horizontalWinningRules() = listOf(
             listOf(Tile.TOP_LEFT, Tile.TOP_MIDDLE, Tile.TOP_RIGHT),
             listOf(Tile.MIDDLE_LEFT, Tile.MIDDLE_MIDDLE, Tile.MIDDLE_RIGHT),
             listOf(Tile.BOTTOM_LEFT, Tile.BOTTOM_MIDDLE, Tile.BOTTOM_RIGHT)
+        )
+
+        @JvmStatic
+        fun verticalWinningRules() = listOf(
+            listOf(Tile.TOP_LEFT, Tile.MIDDLE_LEFT, Tile.BOTTOM_LEFT),
+            listOf(Tile.TOP_MIDDLE, Tile.MIDDLE_MIDDLE, Tile.BOTTOM_MIDDLE),
+            listOf(Tile.TOP_RIGHT, Tile.MIDDLE_RIGHT, Tile.BOTTOM_RIGHT),
         )
     }
 }
