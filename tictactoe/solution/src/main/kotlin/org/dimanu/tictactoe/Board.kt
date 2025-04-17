7213c593211b0518dcd04d3fdffa344ab2f48cdd
dimanu-py <diegomtz126@gmail.com>
2025-04-17 12:55:36 +0200

    ♻ refactor(tictactoe): change tiles storage to map players to tiles and add method to retrieve tiles played by a player

--- tictactoe/solution/src/main/kotlin/org/dimanu/tictactoe/Board.kt
+++ tictactoe/solution/src/main/kotlin/org/dimanu/tictactoe/Board.kt
@@ -1,11 +1,19 @@
 package org.dimanu.tictactoe
 
 class Board {
-    private var tiles: MutableList<Tile> = mutableListOf()
+    private var tiles: MutableMap<Tile, Player> = mutableMapOf()
 
     fun isMarked(tile: Tile): Boolean = tiles.contains(tile)
 
-    fun playOn(tile: Tile) = tiles.add(tile)
-    fun playOn(tile: Tile, player: Player) = tiles.add(tile)
+    fun playOn(tile: Tile, player: Player) = player.also { tiles[tile] = it }
 
+    fun tilesPlayedBy(player: Player): List<Tile> {
+        val playedTiles: MutableList<Tile> = mutableListOf()
+        for (tile in tiles.entries) {
+            if (tile.value == player) {
+                playedTiles.add(tile.key)
+            }
+        }
+        return playedTiles
+    }
 }
