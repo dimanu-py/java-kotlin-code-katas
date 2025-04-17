c86344dd48f9de3cd27014e90dd12d5f46f7d554
dimanu-py <diegomtz126@gmail.com>
2025-04-17 12:25:41 +0200

    ♻ refactor(tictactoe): clean up tests by extracting setUp method to instantiate game

--- tictactoe/solution/src/test/kotlin/org/dimanu/tictactoe/GameShould.kt
+++ tictactoe/solution/src/test/kotlin/org/dimanu/tictactoe/GameShould.kt
@@ -1,21 +1,25 @@
 package org.dimanu.tictactoe
 
+import org.junit.jupiter.api.BeforeEach
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class GameShould {
 
+    private lateinit var game: Game
+
+    @BeforeEach
+    fun setUp() {
+        game = Game()
+    }
+
     @Test
     fun `wait for player X to play first`() {
-        val game = Game()
-
         assertEquals(Player.PLAYER_X, game.toPlay())
     }
 
     @Test
     fun `wait for player O to play after player X`() {
-        val game = Game()
-
         game.play(Tile.TOP_LEFT)
 
         assertEquals(Player.PLAYER_O, game.toPlay())
@@ -23,8 +27,6 @@ class GameShould {
 
     @Test
     fun `wait for player X to play after player O`() {
-        val game = Game()
-
         game.play(Tile.TOP_LEFT)
         game.play(Tile.TOP_MIDDLE)
 
@@ -33,8 +35,6 @@ class GameShould {
 
     @Test
     fun `not allow players to play on already marked tiles`() {
-        val game = Game()
-
         game.play(Tile.TOP_LEFT)
         game.play(Tile.TOP_LEFT)
 
