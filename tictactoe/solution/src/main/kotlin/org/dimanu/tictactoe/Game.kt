cef5c41487fbeeeb0526614dccf5e136ad33491c
dimanu-py <diegomtz126@gmail.com>
2025-04-17 11:46:23 +0200

    ✨ feat(tictactoe): implement Game class and Player enum with fake implementation for test to pass

---
+++ tictactoe/solution/src/main/kotlin/org/dimanu/tictactoe/Game.kt
@@ -0,0 +1,7 @@
+package org.dimanu.tictactoe
+
+class Game {
+    fun toPlay(): Player {
+        return Player.PLAYER_X
+    }
+}
---
+++ tictactoe/solution/src/main/kotlin/org/dimanu/tictactoe/Player.kt
@@ -0,0 +1,5 @@
+package org.dimanu.tictactoe
+
+enum class Player {
+    PLAYER_X
+}
